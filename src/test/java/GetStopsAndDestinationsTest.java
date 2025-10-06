ee9da4ea860f2d9a4102f6716d087899c95edf9d
MikelP7 <mikel.03@live.com>
2025-10-06 02:14:32 +0200

    Añadidos test caja blanca

--- src/test/java/GetStopsAndDestinationsTest.java
+++ src/test/java/GetStopsAndDestinationsTest.java
@@ -26,14 +26,161 @@ public class GetStopsAndDestinationsTest {
 
 	private Driver driver; 
 	
+//	@Test
+//	public void test1CB() throws ParseException {
+//		
+//		String driverEmail="driverTest1@gmail.com";
+//		String driverName="DriverTest1";
+//		String driverPassword="test1";
+//		
+//		Date rideDate = Date.valueOf("2025-10-10");;
+//		
+//		float price = (float) 20.50;
+//		
+//		List<String> res = new ArrayList<String>();
+//		
+//		try {
+//			
+//			testDA.open();
+//			
+//			Driver d = testDA.createDriver(driverEmail, driverPassword, driverName);
+//			
+//			Ride r1 = new Ride("Don", "Bil", rideDate, 4, price, d);
+//			Ride r2 = new Ride("Bil", "Gas", rideDate, 4, price, d); 
+//			Ride r3 = new Ride("Don", "Gas", rideDate, 4, price, d); 
+//			
+//			testDA.addRideToDriver(driverEmail, r1);
+//			testDA.addRideToDriver(driverEmail, r2);
+//			testDA.addRideToDriver(driverEmail, r3);
+//			
+//			testDA.addStopToRide(r1, "Tol", price, 1);
+//			testDA.addStopToRide(r1, "Vil", price, 2);
+//			
+//			testDA.addStopToRide(r2, "Don", price, 1);
+//			testDA.addStopToRide(r2, "Iru", price, 2);
+//			testDA.addStopToRide(r2, "Tol", price, 3);
+//			
+//			testDA.addStopToRide(r3, "Don", price, 1);
+//			testDA.addStopToRide(r3, "Iru", price, 2);
+//			testDA.addStopToRide(r3, "Tol", price, 3);
+//			
+//			testDA.close();			
+//			
+//			sut.open();
+//		    res = sut.getStopsAndDestinations("Don");
+//			sut.close();
+//			System.out.println(res);
+//			
+//			if(res.contains("Tol") && res.contains("Bil") && res.contains("Gas") && res.contains("Vil") && res.contains("Iru")) {
+//				assertTrue(true);
+//			}
+//			else {
+//				fail();
+//			}
+//			
+//		} 
+//		catch (Exception e) {
+//			System.out.println(e.toString());
+//			fail();
+//		} 
+//		
+//		finally {
+//				testDA.open();
+//				
+//				testDA.removeDriver(driverEmail);
+//				
+//		        testDA.close();
+//		}
+//	} 
+//	
+//	
+//	@Test
+//	public void test2CB() {
+//		
+//		List<String> res = new ArrayList<String>();
+//		
+//		try {
+//			sut.open();
+//		    res = sut.getStopsAndDestinations("Bil");
+//			sut.close();
+//			
+//			if(res.isEmpty()) {
+//				assertTrue(true);	
+//			}
+//			else {
+//				fail();
+//			}
+//		} 
+//		catch (Exception e) {
+//			System.out.println(e.toString());
+//			fail();
+//		} 
+//	}
+//	
+//	
+//	@Test
+//	public void test3CB() throws ParseException {
+//		
+//
+//		String driverEmail="driverTest1@gmail.com";
+//		String driverName="DriverTest1";
+//		String driverPassword="test1";
+//		
+//		Date rideDate = Date.valueOf("2025-10-10");;
+//		
+//		float price = (float) 20.50;
+//		
+//		List<String> res = new ArrayList<String>();
+//		
+//		try {
+//			
+//			testDA.open();
+//			
+//			Driver d = testDA.createDriver(driverEmail, driverPassword, driverName);
+//			
+//			Ride r4 = new Ride("Don", "Bil", rideDate, 4, price, d);
+//			
+//			testDA.addRideToDriver(driverEmail, r4);
+//
+//			testDA.close();			
+//			
+//			sut.open();
+//		    res = sut.getStopsAndDestinations("Don");
+//			sut.close();
+//			System.out.println(res);
+//			
+//			if(res.contains("Bil")) {
+//				assertTrue(true);
+//			}
+//			else {
+//				fail();
+//			}
+//			
+//		} 
+//		catch (Exception e) {
+//			System.out.println(e.toString());
+//			fail();
+//		} 
+//		
+//		finally {
+//				testDA.open();
+//				
+//				testDA.removeDriver(driverEmail);
+//				
+//		        testDA.close();
+//		}
+//
+//	}
+//	
+	
 	@Test
-	public void test1() throws ParseException {
+	public void test4CB() throws ParseException {
 		
+
 		String driverEmail="driverTest1@gmail.com";
 		String driverName="DriverTest1";
 		String driverPassword="test1";
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Date rideDate = Date.valueOf("2025-10-10");;
 		
 		float price = (float) 20.50;
@@ -46,103 +193,13 @@ public class GetStopsAndDestinationsTest {
 			
 			Driver d = testDA.createDriver(driverEmail, driverPassword, driverName);
 			
-			Ride r1 = new Ride("Don", "Bil", rideDate, 4, price, d);
 			Ride r2 = new Ride("Bil", "Gas", rideDate, 4, price, d); 
-			Ride r3 = new Ride("Don", "Gas", rideDate, 4, price, d); 
 			
-			testDA.addRideToDriver(driverEmail, r1);
 			testDA.addRideToDriver(driverEmail, r2);
-			testDA.addRideToDriver(driverEmail, r3);
-			
-			testDA.addStopToRide(r1, "Tol", price, 1);
-			testDA.addStopToRide(r1, "Vil", price, 2);
 			
 			testDA.addStopToRide(r2, "Don", price, 1);
 			testDA.addStopToRide(r2, "Iru", price, 2);
 			testDA.addStopToRide(r2, "Tol", price, 3);
-			
-			testDA.addStopToRide(r3, "Don", price, 1);
-			testDA.addStopToRide(r3, "Iru", price, 2);
-			testDA.addStopToRide(r3, "Tol", price, 3);
-			
-			testDA.close();			
-			
-			sut.open();
-		    res = sut.getStopsAndDestinations("Don");
-			sut.close();
-			System.out.println(res);
-			
-			if(res.contains("Tol") && res.contains("Bil") && res.contains("Gas") && res.contains("Vil") && res.contains("Iru")) {
-				assertTrue(true);
-			}
-			else {
-				fail();
-			}
-			
-		} 
-		catch (Exception e) {
-			System.out.println(e.toString());
-			fail();
-		} 
-		
-		finally {
-				testDA.open();
-				
-				testDA.removeDriver(driverEmail);
-				
-		        testDA.close();
-		}
-	} 
-	
-	
-	@Test
-	public void test2() {
-		
-		List<String> res = new ArrayList<String>();
-		
-		try {
-			sut.open();
-		    res = sut.getStopsAndDestinations("Bil");
-			sut.close();
-			
-			if(res.isEmpty()) {
-				assertTrue(true);	
-			}
-			else {
-				fail();
-			}
-		} 
-		catch (Exception e) {
-			System.out.println(e.toString());
-			fail();
-		} 
-	}
-	
-	
-	@Test
-	public void test3() throws ParseException {
-		
-
-		String driverEmail="driverTest1@gmail.com";
-		String driverName="DriverTest1";
-		String driverPassword="test1";
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date rideDate = Date.valueOf("2025-10-10");;
-		
-		float price = (float) 20.50;
-		
-		List<String> res = new ArrayList<String>();
-		
-		try {
-			
-			testDA.open();
-			
-			Driver d = testDA.createDriver(driverEmail, driverPassword, driverName);
-			
-			Ride r4 = new Ride("Don", "Bil", rideDate, 4, price, d);
-			
-			testDA.addRideToDriver(driverEmail, r4);
 
 			testDA.close();			
 			
@@ -151,7 +208,7 @@ public class GetStopsAndDestinationsTest {
 			sut.close();
 			System.out.println(res);
 			
-			if(res.contains("Bil")) {
+			if(res.contains("Iru") && res.contains("Tol")) {
 				assertTrue(true);
 			}
 			else {
